346e734f7932e9e515fde008ccb9d9da2378ab07
Sam Ardis <ardis.productions@gmail.com>
2020-02-21 12:32:10 -0500

    Added StarWarsPage content and stlying as well as LoadingPage, fixed issues with other pages, and started on stretch.

--- starwars/src/components/LoadingPage.js
+++ starwars/src/components/LoadingPage.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const LoadingDiv = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+height: 100vh;
+position: relative;
+`;
+
 function LoadingPage() {
-
     return (
-
+        <LoadingDiv>
+            <img src='https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif'
+            alt='Loading'/>
+        </LoadingDiv>
     );
 };
 
--- starwars/src/components/StarWarsPage.js
+++ starwars/src/components/StarWarsPage.js
@@ -2,12 +2,32 @@ import React from 'react';
 import { Card, CardHeader, CardBody, CardTitle } from 'reactstrap';
 import styled from 'styled-components';
 
+const StarDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    max-wdith: 315px;
+    min-width: 315px;
+    padding: 15px 15px 0 0;
+`;
 
-
-const StarWarsPage(props) {
+const StarWarsPage = (props) => {
     return (
-
-    )
+        <StarDiv>
+            <Card>
+                <CardHeader>Name: {props.name}</CardHeader>
+                <CardBody>
+                    <CardTitle>Birthday: {props.birth_year}</CardTitle>
+                    <CardTitle>Gender: {props.gender}</CardTitle>
+                    <CardTitle>Height: {props.height}</CardTitle>
+                    <CardTitle>Mass: {props.mass}</CardTitle>
+                    <CardTitle>Hair Color: {props.hair_color}</CardTitle>
+                    <CardTitle>Skin Color: {props.skin_color}</CardTitle>
+                    <CardTitle>Eye Color: {props.eye_color}</CardTitle>
+                </CardBody>
+            </Card>
+        </StarDiv>
+    );
 
 };
 
