9efcd805fbd872a5cd99c13b8245695ec961ccbf
Sam Ardis <ardis.productions@gmail.com>
2020-02-21 12:07:32 -0500

    Added index.js bootstrap and App.js and started other .js pages

---
+++ starwars/src/components/StarWarsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card, CardHeader, CardBody, CardTitle } from 'reactstrap';
+import styled from 'styled-components';
+
+
+
+const StarWarsPage(props) {
+    return (
+
+    )
+
+};
+
+export default StarWarsPage;
